086b517352e60f26fca8dfb47619b5f170a40238
Dustin Kelley <kelleydustin2018@gmail.com>
2022-07-12 16:21:00 -0500

    configured client.js to integtrate back end

--- components/HeroBanner.js
+++ components/HeroBanner.js
@@ -1,29 +1,29 @@
-import React from 'react'
+import React from 'react';
 import Link from 'next/link';
 
-const HeroBanner = () => {
-  return (
-    <div className='hero-banner-container' >
-      <div>
-        <p className='beats-solo'>SMALL TEXT HERE</p>
-     
-      <h3>MID TEXT</h3>
-      <img className='hero-banner-image' src=''  alt='headphones'/>
+import { urlFor } from '../lib/client';
 
+const HeroBanner = ({ heroBanner }) => {
+  return (
+    <div className="hero-banner-container">
       <div>
-        <Link href='/product/ID'>
-          <button type='button'>BUTTON TEXT</button>
-        </Link>
-        <div className='desc'>
-          <h5>Description</h5>
-          <p>DESCRIPTION</p>
-        </div>
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img src={urlFor(heroBanner.image)} alt="headphones" className="hero-banner-image" />
+
+        <div>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
+          </Link>
+          <div className="desc">
+            <h5>Description</h5>
+            <p>{heroBanner.desc}</p>
+          </div>
         </div>
       </div>
     </div>
-
-
   )
 }
 
-export default HeroBanner;
+export default HeroBanner
